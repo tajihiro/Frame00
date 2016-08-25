8eb3aeaf7694766a4239654dd4604e090bd7368c
H.Tajima <tajihiro2@gmail.com>
2016-08-26 00:17:18 +0900

    Added page crawl.

--- sample06.rb
+++ sample06.rb
@@ -1,7 +1,14 @@
 require 'anemone'
 
-Anemone.crawl('http://www.whoocus.com/blog', :depth_limit => 1) do |anemone|
+Anemone.crawl('https://www.amazon.co.jp/gp/bestsellers/books/', :depth_limit => 1) do |anemone|
+  anemone.focus_crawl do |page|
+    page.links.keep_if { |link|
+      link.to_s.match(/\/gp\/bestsellers\/books\/.?/)
+    }
+  end
+
   anemone.on_every_page do |page|
     puts page.url
   end
+
 end
